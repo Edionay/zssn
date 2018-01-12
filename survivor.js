debde4933ec528984515c0d531878fb115f62518
Edionay <edionay@gmail.com>
2018-01-12 03:33:15 -0200

    First working draft of the survivor insertion feature.

---
+++ survivor.js
@@ -0,0 +1,17 @@
+class Survivor {
+    constructor (name, age, gender, location, food, water, food, medication, ammunition, isInfected) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.location = location;
+        this.inventory.water;
+        this.inventory.food;
+        this.inventory.medication;
+        this.inventory.ammunition;
+        this.infected = isInfected;
+    }
+
+    updateLocation (newLocation) {
+        this.location = newLocation;
+    }
+}
---
+++ zssn.js
@@ -0,0 +1,51 @@
+window.addEventListener("load", function() { 
+    requestSurvivors();
+})
+
+function requestSurvivors() {
+    const requestUrl = 'http://zssn-backend-example.herokuapp.com/api/people.json';
+    const request = new XMLHttpRequest;
+    
+    request.open('GET', requestUrl);
+    request.responseType = 'json';
+    request.onload = () => showSurvivors(request.response);
+    request.send();
+}
+
+function showSurvivors(survivorsList) {
+    const survivorsTable = document.getElementById('survivors_list');
+    for (const survivor of survivorsList) {
+        const newRow = survivorsTable.insertRow();
+        let nameCell = newRow.insertCell(0);
+        let ageCell = newRow.insertCell(1);
+        let genderCell = newRow.insertCell(2);
+        let infectedCell = newRow.insertCell(3);
+        let locationCell = newRow.insertCell(4);
+
+        nameCell.innerText = survivor.name;
+        ageCell.innerText = survivor.age;
+        genderCell.innerText = survivor.gender;
+        infectedCell.innerText = survivor['infected?'] ? 'Yes' : 'No';
+        locationCell.innerText = survivor.lonlat;
+    }
+}
+
+function toggleNewSurvivorForm() {
+    const newSurvivorForm = document.getElementById('new_survivor_form');
+    if (newSurvivorForm.style.display === 'block') {
+        newSurvivorForm.style.display = 'none';
+    } else {
+        newSurvivorForm.style.display = 'block';
+    }
+}
+
+function fillItemsInput() {
+    const form = document.getElementById('new_survivor_form');
+    const water = document.getElementById('water');
+    const food = document.getElementById('food');
+    const medicine = document.getElementById('medicine');
+    const ammunition = document.getElementById('ammunition');
+    const items = "Water:"+water+";Food:"+food+";Medicine:"+medicine+";Ammunition:"+ammunition;
+    form.elements.namedItem('items').value = items;
+    return true;
+}
