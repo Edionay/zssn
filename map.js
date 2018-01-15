6be861e3238d40c8c2886c7925c9d259fc70e203
Edionay <edionay@gmail.com>
2018-01-15 05:58:16 -0200

    Now you can pick location from Google Maps.

---
+++ map.js
@@ -0,0 +1,47 @@
+function initMap() {
+    var defaultPosition = {lat: -16.7022998021961, lng: -49.26475524902344};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: defaultPosition
+    });    
+
+        // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        map.setCenter(pos);
+        var marker = new google.maps.Marker({
+            position: pos,
+            map: map,
+            draggable: true
+        });
+
+        fillLocationFields(pos);
+    
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    map.addListener('click', event => {
+        placeMarkerAndPanTo(event.latLng, map);
+        fillLocationFields({lat: event.latLng.lat(), lng: event.latLng.lng()});
+        console.log(event.latLng.lat());
+        console.log(event.latLng.lng());
+      });
+      
+}
+function placeMarkerAndPanTo(latLng, map) {
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+    map.panTo(latLng);
+}
--- zssn.js
+++ zssn.js
@@ -47,5 +47,19 @@ function fillItemsInput() {
     const ammunition = document.getElementById('ammunition').value;
     const items = "Water:"+water+";Food:"+food+";Medicine:"+medication+";Ammunition:"+ammunition;
     form.elements.namedItem('items').value = items;
+    const lat = document.getElementById('latitude').innerText;
+    const lon = document.getElementById('longitude').innerText;
+    form.elements.namedItem('person[lonlat]').value = parseLatlonToPointString({lat, lon});
     return true;
+}
+
+function fillLocationFields(latlon) {
+    let longitudeField = document.getElementById('longitude');
+    longitudeField.innerText = latlon.lng;
+    let latitudeField = document.getElementById('latitude');
+    latitudeField.innerText = latlon.lat;
+}
+
+function parseLatlonToPointString(latlon) {
+    return `Point(${latlon.lon} ${latlon.lat})`;
 }
